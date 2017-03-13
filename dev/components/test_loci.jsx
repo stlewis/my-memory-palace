7a38885203a9b1a761aaa90500c59932475ef338
Steve Lewis <steve@decodingsteve.com>
2017-03-13 13:01:14 -0700

    Fix bug causing loci to keep listing

--- dev/components/test_loci.jsx
+++ dev/components/test_loci.jsx
@@ -14,8 +14,8 @@ var TestLoci = React.createClass({
       // If they were right, increment the loci counter, provided there's something in the 
       // next loci. If there isn't, they've completed the list, so bounce
       var nextLociIdx = this.state.loci_index + 1;
-      var nextLoci = this.state.loci[nextLociIdx];
-      if (nextLoci != null){
+      var nextLoci = this.state.loci[nextLociIdx].value;
+      if (nextLoci != ""){
         this.setState({loci_index: nextLociIdx});
       }else{
         this.setState({message: "You got them all, congratulations!"});
